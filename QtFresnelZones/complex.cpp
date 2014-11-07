2e4334c717cc1d0c39f1d40ccb8d9a2f40a3e6da
snakerock <snake.rock7@gmail.com>
2014-11-07 12:13:32 +0300

    + Code styled and optimized; + Detailed comments added to fresnel.h, PLEASE READ;

--- QtFresnelZones/complex.cpp
+++ QtFresnelZones/complex.cpp
@@ -2,32 +2,32 @@
 
 #include <cmath>
 
-Complex::Complex(double re,
-                 double im) :
-    re(re),
-    im(im)
+Complex::Complex (double re,
+                  double im) :
+    re (re),
+    im (im)
 {
 
 }
 
-double Complex::sqAbs() {
-    return this->re * this->re + this->im * this->im;
+double Complex::sqAbs () {
+    return re*re + im*im;
 }
 
-double Complex::abs() {
-    return sqrt(this->sqAbs());
+double Complex::abs () {
+    return sqrt (sqAbs ());
 }
 
-Complex Complex::operator *=(double k)
+Complex Complex::operator *= (double k)
 {
-    this->re *= k;
-    this->im *= k;
+    re *= k;
+    im *= k;
     return *this;
 }
 
-Complex Complex::operator +=(Complex c)
+Complex Complex::operator += (Complex c)
 {
-    this->re += c.re;
-    this->im += c.im;
+    re += c.re;
+    im += c.im;
     return *this;
 }
--- QtFresnelZones/fresnel.cpp
+++ QtFresnelZones/fresnel.cpp
@@ -2,242 +2,253 @@
 #include <QDebug>
 #include <cmath>
 
-Fresnel::Fresnel()
+Fresnel::Fresnel ()
 {
-    this->setDefaults();
+    setDefaults ();
 }
 
-void Fresnel::setDefaults()
+void Fresnel::setDefaults ()
 {
-    this->initialAmplitude    = 100;                        // E0
-    this->waveLength          = wave_def;                   // l
-    this->holeRadius          = radius_def;                 // R
-    this->observerDistance    = dist_def;                   // b
-    this->sourceDistance      = 0.05;                       // a
-    this->accuracyPlot        = 0.0001;                     // dr in plot integral
-    this->accuracySpiral      = 10;                         // Number of spiral vectors in one zone
-    this->waveNumber          = 2.0 * M_PI / waveLength;    // k
-    this->refractiveIndex     = 2.0;                        // n
-    this->phasePlateType      = PhasePlate::LENS;
-    this->amplitudePlate      = false;
-    this->phasePlate          = false;
-    this->openedZones         = QVector<bool>(this->fresnelNumber() + 1, true);
+    initialAmplitude      = 100;                        // E0
+    waveLength            = wave_def;                   // l
+    holeRadius            = radius_def;                 // R
+    observerDistance      = dist_def;                   // b
+    sourceDistance        = 0.05;                       // a
+    accuracyPlot          = 0.0001;                     // dr in amplitude integral
+    accuracySpiral        = 10;                         // Number of spiral vectors in one zone
+    waveNumber            = 2.0 * M_PI / waveLength;    // k
+    currentFresnelNumber  = fresnelNumber (holeRadius); // fn
+    refractiveIndex       = 2.0;                        // n
+    phasePlateType        = PhasePlate::LENS;
+    amplitudePlate        = false;
+    phasePlate            = false;
+    openedZones           = QVector<bool> (currentFresnelNumber + 1, true);
 }
 
-double Fresnel::getWaveLength() const
+double Fresnel::getWaveLength () const
 {
     return waveLength;
 }
 
-void Fresnel::setWaveLength(double value)
+void Fresnel::setWaveLength (double value)
 {
-    int oldFN = fresnelNumber();
-    waveLength = value;
-    waveNumber = 2.0 * M_PI / waveLength;
-    updateAmplitudePlate(oldFN);
+    int oldFN            = currentFresnelNumber;
+    waveLength           = value;
+    waveNumber           = 2.0 * M_PI / waveLength;
+    currentFresnelNumber = fresnelNumber (holeRadius);
+    updateAmplitudePlate (oldFN);
 }
 
-double Fresnel::getHoleRadius() const
+double Fresnel::getHoleRadius () const
 {
     return holeRadius;
 }
 
-void Fresnel::setHoleRadius(double value)
+void Fresnel::setHoleRadius (double value)
 {
-    int oldFN = fresnelNumber();
-    holeRadius = value;
-    updateAmplitudePlate(oldFN);
+    int oldFN            = currentFresnelNumber;
+    holeRadius           = value;
+    currentFresnelNumber = fresnelNumber (holeRadius);
+    updateAmplitudePlate (oldFN);
 }
 
-double Fresnel::getObserverDistance() const
+double Fresnel::getObserverDistance () const
 {
     return observerDistance;
 }
 
-void Fresnel::setObserverDistance(double value)
+void Fresnel::setObserverDistance (double value)
 {
-    int oldFN = fresnelNumber();
-    observerDistance = value;
-    updateAmplitudePlate(oldFN);
+    int oldFN            = currentFresnelNumber;
+    observerDistance     = value;
+    currentFresnelNumber = fresnelNumber (holeRadius);
+    updateAmplitudePlate (oldFN);
 }
 
-bool Fresnel::isZoneOpened(unsigned i)
+void Fresnel::updateAmplitudePlate (int oldFN)
 {
-    return i < openedZones.count() ? openedZones[i] : false;
+    if (oldFN < 0) {
+        return;
+    }
+
+    if (currentFresnelNumber > oldFN) {
+        for (int i = 0; i < currentFresnelNumber - oldFN; ++i) {
+            openedZones.push_back (true);
+        }
+    } else {
+        //openedZones.remove (openedZones.count () - (oldFN - currentFresnelNumber), oldFN - currentFresnelNumber);
+    }
 }
 
-void Fresnel::setZoneOpenness(unsigned i, bool isOpen)
+bool Fresnel::isZoneOpened (int i) const
 {
-    if (i < openedZones.count()) {
+    return i >= 0 && i < openedZones.count () ? openedZones[i] : false;
+}
+
+void Fresnel::setZoneOpenness (int i, bool isOpen)
+{
+    if (i >= 0 && i < openedZones.count ()) {
         openedZones[i] = isOpen;
     }
 }
 
-void Fresnel::updateAmplitudePlate(int oldFN)
+double Fresnel::intensity () const
 {
-    int currentFN = fresnelNumber();
-
-    if (currentFN > oldFN) {
-        for (int i = 0; i < currentFN - oldFN; ++i) {
-            openedZones.push_back(true);
-        }
-    } else {
-        openedZones.remove(openedZones.count() - (oldFN - currentFN), oldFN - currentFN);
-    }
-}
-
-double Fresnel::intensity()
-{
-    Complex amp = this->amplitude();
-    return amp.sqAbs() * 3e+11 / (8.0 * M_PI);
+    Complex amp = amplitude ();
+    return amp.sqAbs () * 3e+11 / (8.0 * M_PI);
 }
 
 //
 // Integrate from innerR to outerR:
 // PI / l * E0 * e^(-k * (d - b)) / d * r * (cos(phi) + 1)) * dr
 //
-Complex Fresnel::amplitude(double innerR,
-                           double outerR)
+Complex Fresnel::amplitude (double innerR,
+                            double outerR) const
 {
     if (outerR < 0) {
-        outerR = this->holeRadius;
+        outerR = holeRadius;
     }
 
     Complex amp;
-    unsigned n;
-    double r, dr, d, arg, p, phi;
-    double R = outerR - innerR;
+    double r, d, arg, p, phi;
 
-    dr = this->accuracyPlot < R ? this->accuracyPlot : R / 2.0;
-    n = R / dr + 1;
-    dr = R / n;
-
-    double l = this->waveLength;
-    double a = this->sourceDistance;
-    double k = this->waveNumber;
-    double b = this->observerDistance;
-    double b2 = b * b;
+    double R    = outerR - innerR;
+    int    n    = R / (accuracyPlot < R ? accuracyPlot : R / 2.0) + 1;
+    double dr   = R / n;
+    double l    = waveLength;
+    double a    = sourceDistance;
+    double k    = waveNumber;
+    double b    = observerDistance;
+    double b2   = b * b;
     double intK = M_PI / l * dr;
 
-    for (unsigned i = 0; i < n; ++i) {
+    for (int i = 0; i < n; ++i) {
         r = innerR + i * dr;
-        d = sqrt(r*r + b2);
-        phi = atan(r / a) + atan(r / b);
+        d = sqrt (r*r + b2);
+        phi = atan (r / a) + atan (r / b);
 
-        p = 1.0;
-        p = 1.0 / d;            // Sphere wave factor
-        p *= r;                 // Jacobian
-        p *= cos(phi) + 1.0;    // K(phi)
-        p *= amplitudeOnPlate(r);
+        p = 1.0 / d;                // Sphere wave factor
+        p *= r;                     // Jacobian
+        p *= cos (phi) + 1.0;       // K(phi)
+        p *= amplitudeOnPlate (r);
 
-        arg = -k * (d - b) + phaseOnPlate(r);     // Phase
-        amp.re += -p * sin(arg);
-        amp.im += p * cos(arg);
+        arg = -k * (d - b) + phaseOnPlate (r);     // Phase
+        amp.re += -p * sin (arg);
+        amp.im += p * cos (arg);
     }
 
     amp *= intK;
     return amp;
 }
 
-double Fresnel::amplitudeOnPlate(double r)
+double Fresnel::amplitudeOnPlate (double r) const
 {
-    unsigned fn = fresnelNumber(r);
-    if (amplitudePlate && fn < openedZones.count()) {
+    int fn = fresnelNumber (r);
+
+    if (amplitudePlate && fn < openedZones.count ()) {
         return openedZones[fn] ? initialAmplitude : 0.0;
     } else {
         return initialAmplitude;
     }
 }
 
-double Fresnel::phaseOnPlate(double r)
+double Fresnel::phaseOnPlate (double r) const
 {
     if (!phasePlate) {
         return 0.0;
     }
 
-    double n = refractiveIndex;
-    double k = waveNumber;
-    double deltaPhase = n * k * getPhasePlateWidthOnRing(r);
+    double n          = refractiveIndex;
+    double k          = waveNumber;
+    double deltaPhase = n * k * getPhasePlateWidthOnRing (r);
 
     return deltaPhase;
 }
 
-double Fresnel::getPhasePlateWidthOnRing(double r)
+double Fresnel::getPhasePlateWidthOnRing (double r) const
 {
-    double n = refractiveIndex;
-    double k = waveNumber;
-    double b = observerDistance;
+    r = fabs (r);
+
+    double n     = refractiveIndex;
+    double k     = waveNumber;
+    double b     = observerDistance;
     double width = 0.0;
 
     if (phasePlateType == PhasePlate::SIMPLE) {
-        unsigned zoneNumber = fresnelNumber(r);
+        int zoneNumber = fresnelNumber (r);
         if (zoneNumber & 1) {
             width = M_PI / n / k;
         }
     }
 
     if (phasePlateType == PhasePlate::STAGING) {
-        unsigned fn = fresnelNumber();
-        unsigned zoneNumber = fresnelNumber(r);
-        double dWidth = M_PI / n / k;
+        int fn         = currentFresnelNumber;
+        int zoneNumber = fresnelNumber (r);
+        double dWidth  = M_PI / n / k;
 
-        width = (fn - zoneNumber + fn & 1 + 2) * dWidth;
+        width = (fn - zoneNumber + (fn & 1) + 2) * dWidth;
     }
 
     if (phasePlateType == PhasePlate::LENS) {
-        width = (3.0 / 2.0 * M_PI + k * (sqrt(b*b + r*r) - b)) / n / k;
+        width = (3.0 / 2.0 * M_PI + k * (sqrt (b*b + r*r) - b)) / n / k;
     }
 
     if (phasePlateType == PhasePlate::FLAT_LENS) {
-        width = fmod(3.0 / 2.0 * M_PI + k * (sqrt(b*b + r*r) - b), 2.0 * M_PI) / n / k;
+        width = fmod (3.0 / 2.0 * M_PI + k * (sqrt (b*b + r*r) - b), 2.0 * M_PI) / n / k;
     }
 
     return width;
 }
 
-double Fresnel::zoneOuterRadius(unsigned n)
+double Fresnel::zoneOuterRadius (int n) const
 {
-    double b = this->observerDistance;
-    double l = this->waveLength;
-    return sqrt((n + 1)*(n + 1) * l*l / 4.0 + l * (n + 1) * b);
+    if (n < 0) {
+        return 0.0;
+    }
+
+    double b = observerDistance;
+    double l = waveLength;
+    return sqrt ((n + 1)*(n + 1) * l*l / 4.0 + l * (n + 1) * b);
 }
 
-unsigned Fresnel::fresnelNumber(double r)
+int Fresnel::fresnelNumber (double r) const
 {
     if (r < 0) {
-        r = this->holeRadius;
+        return currentFresnelNumber;
     }
-    double b = this->observerDistance;
-    double l = this->waveLength;
-    return (unsigned int) (2.0 * (sqrt(b*b + r*r) - b) / l);
+    double b = observerDistance;
+    double l = waveLength;
+    return (int) (2.0 * (sqrt (b*b + r*r) - b) / l);
 }
 
-void Fresnel::spiral(DoubleVector &spiralX, DoubleVector &spiralY)
+void Fresnel::spiral (DoubleVector &spiralX, DoubleVector &spiralY) const
 {
-    spiralX.clear();
-    spiralY.clear();
-    spiralX.push_back(0.0);
-    spiralY.push_back(0.0);
+    spiralX.clear ();
+    spiralY.clear ();
+    spiralX.push_back (0.0);
+    spiralY.push_back (0.0);
 
-    unsigned fn = this->fresnelNumber();
-    double R = this->holeRadius;
-    double innerR = 0.0;
-    double outerR = 0.0;
-    double dr = 0.0;
     Complex sp;
 
-    for (unsigned n = 0; n < fn + 1; ++n) {
-        outerR = this->zoneOuterRadius(n);
+    int fn        = currentFresnelNumber;
+    double R      = holeRadius;
+    double innerR = 0.0;
+    double outerR = 0.0;
+    double dr     = 0.0;
+
+    for (int n = 0; n < fn + 1; ++n) {
+        outerR = zoneOuterRadius (n);
         dr = (outerR - innerR) / accuracySpiral;
 
-        for (unsigned i = 0; i < accuracySpiral; ++i) {
+        for (int i = 0; i < accuracySpiral; ++i) {
             if (innerR > R) {
                 break;
             }
 
-            sp += this->amplitude(innerR, innerR + dr);
-            spiralX.push_back(sp.im);
-            spiralY.push_back(sp.re);
+            sp += amplitude (innerR, innerR + dr);
+            spiralX.push_back (sp.im);
+            spiralY.push_back (sp.re);
+
             innerR += dr;
         }
     }
