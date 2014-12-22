9012bdc74c9b1a9c1d1c2a8aa9765d5483ffc40a
snakerock <snake.rock7@gmail.com>
2014-12-22 10:24:20 +0300

    * English translations;

--- build-QtFresnelZones-Desktop-Debug/ui_phaseplatewindow.h
+++ build-QtFresnelZones-Desktop-Debug/ui_phaseplatewindow.h
@@ -9,6 +9,7 @@
 #ifndef UI_PHASEPLATEWINDOW_H
 #define UI_PHASEPLATEWINDOW_H
 
+#include <QtCore/QLocale>
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
@@ -89,6 +90,7 @@ public:
         gridLayout->setObjectName(QStringLiteral("gridLayout"));
         frame_3 = new QFrame(centralwidget);
         frame_3->setObjectName(QStringLiteral("frame_3"));
+        frame_3->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_3->setFrameShape(QFrame::NoFrame);
         frame_3->setFrameShadow(QFrame::Raised);
         gridLayout_2 = new QGridLayout(frame_3);
@@ -98,6 +100,7 @@ public:
         gridLayout_2->setContentsMargins(0, 0, 0, 0);
         frame = new QFrame(frame_3);
         frame->setObjectName(QStringLiteral("frame"));
+        frame->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame->setFrameShape(QFrame::NoFrame);
         frame->setFrameShadow(QFrame::Raised);
         verticalLayout = new QVBoxLayout(frame);
@@ -105,6 +108,7 @@ public:
         verticalLayout->setContentsMargins(0, 0, 0, 0);
         frame_5 = new QFrame(frame);
         frame_5->setObjectName(QStringLiteral("frame_5"));
+        frame_5->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_5->setFrameShape(QFrame::StyledPanel);
         frame_5->setFrameShadow(QFrame::Raised);
         horizontalLayout = new QHBoxLayout(frame_5);
@@ -118,6 +122,7 @@ public:
         sizePolicy.setVerticalStretch(0);
         sizePolicy.setHeightForWidth(widget_schemeGraph->sizePolicy().hasHeightForWidth());
         widget_schemeGraph->setSizePolicy(sizePolicy);
+        widget_schemeGraph->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         horizontalLayout->addWidget(widget_schemeGraph);
 
@@ -131,6 +136,7 @@ public:
         sizePolicy1.setVerticalStretch(0);
         sizePolicy1.setHeightForWidth(frame_4->sizePolicy().hasHeightForWidth());
         frame_4->setSizePolicy(sizePolicy1);
+        frame_4->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_4->setFrameShape(QFrame::NoFrame);
         frame_4->setFrameShadow(QFrame::Raised);
         horizontalLayout_2 = new QHBoxLayout(frame_4);
@@ -144,11 +150,13 @@ public:
         sizePolicy2.setVerticalStretch(0);
         sizePolicy2.setHeightForWidth(widget_zonesGraph->sizePolicy().hasHeightForWidth());
         widget_zonesGraph->setSizePolicy(sizePolicy2);
+        widget_zonesGraph->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         horizontalLayout_2->addWidget(widget_zonesGraph);
 
         frame_7 = new QFrame(frame_4);
         frame_7->setObjectName(QStringLiteral("frame_7"));
+        frame_7->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_7->setFrameShape(QFrame::NoFrame);
         frame_7->setFrameShadow(QFrame::Raised);
         verticalLayout_3 = new QVBoxLayout(frame_7);
@@ -161,6 +169,7 @@ public:
         font.setBold(true);
         font.setWeight(75);
         label_3->setFont(font);
+        label_3->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_3->addWidget(label_3);
 
@@ -171,6 +180,7 @@ public:
         sizePolicy3.setVerticalStretch(0);
         sizePolicy3.setHeightForWidth(progressBar_intensity->sizePolicy().hasHeightForWidth());
         progressBar_intensity->setSizePolicy(sizePolicy3);
+        progressBar_intensity->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         progressBar_intensity->setValue(24);
 
         verticalLayout_3->addWidget(progressBar_intensity);
@@ -184,11 +194,13 @@ public:
         sizePolicy3.setHeightForWidth(label_2->sizePolicy().hasHeightForWidth());
         label_2->setSizePolicy(sizePolicy3);
         label_2->setFont(font);
+        label_2->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_3->addWidget(label_2);
 
         frame_8 = new QFrame(frame_7);
         frame_8->setObjectName(QStringLiteral("frame_8"));
+        frame_8->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_8->setFrameShape(QFrame::NoFrame);
         frame_8->setFrameShadow(QFrame::Raised);
         horizontalLayout_3 = new QHBoxLayout(frame_8);
@@ -202,6 +214,7 @@ public:
         sizePolicy4.setVerticalStretch(0);
         sizePolicy4.setHeightForWidth(frame_9->sizePolicy().hasHeightForWidth());
         frame_9->setSizePolicy(sizePolicy4);
+        frame_9->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_9->setFrameShape(QFrame::NoFrame);
         frame_9->setFrameShadow(QFrame::Raised);
         verticalLayout_4 = new QVBoxLayout(frame_9);
@@ -218,6 +231,7 @@ public:
         font1.setFamily(QStringLiteral("Arial"));
         font1.setPointSize(16);
         radioButton_simplePlate->setFont(font1);
+        radioButton_simplePlate->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         radioButton_simplePlate->setChecked(true);
 
         verticalLayout_4->addWidget(radioButton_simplePlate);
@@ -227,6 +241,7 @@ public:
         sizePolicy5.setHeightForWidth(radioButton_stagingPlate->sizePolicy().hasHeightForWidth());
         radioButton_stagingPlate->setSizePolicy(sizePolicy5);
         radioButton_stagingPlate->setFont(font1);
+        radioButton_stagingPlate->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_4->addWidget(radioButton_stagingPlate);
 
@@ -235,6 +250,7 @@ public:
         sizePolicy5.setHeightForWidth(radioButton_lensPlate->sizePolicy().hasHeightForWidth());
         radioButton_lensPlate->setSizePolicy(sizePolicy5);
         radioButton_lensPlate->setFont(font1);
+        radioButton_lensPlate->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_4->addWidget(radioButton_lensPlate);
 
@@ -243,6 +259,7 @@ public:
         sizePolicy5.setHeightForWidth(radioButton_flatLensPlate->sizePolicy().hasHeightForWidth());
         radioButton_flatLensPlate->setSizePolicy(sizePolicy5);
         radioButton_flatLensPlate->setFont(font1);
+        radioButton_flatLensPlate->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_4->addWidget(radioButton_flatLensPlate);
 
@@ -251,6 +268,7 @@ public:
 
         frame_10 = new QFrame(frame_8);
         frame_10->setObjectName(QStringLiteral("frame_10"));
+        frame_10->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_10->setFrameShape(QFrame::NoFrame);
         frame_10->setFrameShadow(QFrame::Raised);
         verticalLayout_5 = new QVBoxLayout(frame_10);
@@ -259,24 +277,28 @@ public:
         label_4 = new FontScalableLabel(frame_10);
         label_4->setObjectName(QStringLiteral("label_4"));
         label_4->setFont(font1);
+        label_4->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_5->addWidget(label_4);
 
         label_5 = new FontScalableLabel(frame_10);
         label_5->setObjectName(QStringLiteral("label_5"));
         label_5->setFont(font1);
+        label_5->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_5->addWidget(label_5);
 
         label_6 = new FontScalableLabel(frame_10);
         label_6->setObjectName(QStringLiteral("label_6"));
         label_6->setFont(font1);
+        label_6->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_5->addWidget(label_6);
 
         label_7 = new FontScalableLabel(frame_10);
         label_7->setObjectName(QStringLiteral("label_7"));
         label_7->setFont(font1);
+        label_7->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_5->addWidget(label_7);
 
@@ -303,6 +325,7 @@ public:
         sizePolicy6.setVerticalStretch(0);
         sizePolicy6.setHeightForWidth(frame_2->sizePolicy().hasHeightForWidth());
         frame_2->setSizePolicy(sizePolicy6);
+        frame_2->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         frame_2->setFrameShape(QFrame::StyledPanel);
         frame_2->setFrameShadow(QFrame::Raised);
         verticalLayout_2 = new QVBoxLayout(frame_2);
@@ -316,6 +339,7 @@ public:
         label_8->setFont(font1);
         label_8->setStyleSheet(QLatin1String("padding-bottom:6;\n"
 ""));
+        label_8->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         label_8->setAlignment(Qt::AlignCenter);
 
         verticalLayout_2->addWidget(label_8);
@@ -327,6 +351,7 @@ public:
         sizePolicy7.setVerticalStretch(0);
         sizePolicy7.setHeightForWidth(widget_spiralGraph->sizePolicy().hasHeightForWidth());
         widget_spiralGraph->setSizePolicy(sizePolicy7);
+        widget_spiralGraph->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
 
         verticalLayout_2->addWidget(widget_spiralGraph);
 
@@ -338,6 +363,7 @@ public:
         sizePolicy3.setHeightForWidth(widget->sizePolicy().hasHeightForWidth());
         widget->setSizePolicy(sizePolicy3);
         widget->setStyleSheet(QStringLiteral(""));
+        widget->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         horizontalLayout_4 = new QHBoxLayout(widget);
         horizontalLayout_4->setSpacing(0);
         horizontalLayout_4->setObjectName(QStringLiteral("horizontalLayout_4"));
@@ -361,6 +387,7 @@ public:
 "padding-bottom:5%;\n"
 "padding-left:5%;\n"
 "padding-right:5%"));
+        pushButton_Back->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         pushButton_Back->setAutoDefault(false);
         pushButton_Back->setDefault(true);
         pushButton_Back->setFlat(false);
@@ -381,6 +408,7 @@ public:
         font3.setUnderline(true);
         font3.setWeight(75);
         label->setFont(font3);
+        label->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         label->setAlignment(Qt::AlignCenter);
 
         horizontalLayout_4->addWidget(label, 0, Qt::AlignHCenter|Qt::AlignVCenter);
@@ -400,6 +428,7 @@ public:
 "padding-bottom:5%;\n"
 "padding-left:5%;\n"
 "padding-right:5%"));
+        pushButton_Next->setLocale(QLocale(QLocale::English, QLocale::UnitedStates));
         pushButton_Next->setFlat(false);
 
         horizontalLayout_4->addWidget(pushButton_Next);
@@ -422,20 +451,20 @@ public:
     void retranslateUi(QMainWindow *PhasePlateWindow)
     {
         PhasePlateWindow->setWindowTitle(QApplication::translate("PhasePlateWindow", "MainWindow", 0));
-        label_3->setText(QApplication::translate("PhasePlateWindow", "\320\230\320\275\321\202\320\265\320\275\321\201\320\270\320\262\320\275\320\276\321\201\321\202\321\214 \320\262 \321\202\320\276\321\207\320\272\320\265 \320\275\320\260\320\261\320\273\321\216\320\264\320\265\320\275\320\270\321\217", 0));
-        label_2->setText(QApplication::translate("PhasePlateWindow", "\320\242\320\270\320\277 \321\204\320\260\320\267\320\276\320\262\320\276\320\271 \320\277\320\273\320\260\321\201\321\202\320\270\320\275\320\272\320\270:", 0));
+        label_3->setText(QApplication::translate("PhasePlateWindow", "Intensity at the point of observation", 0));
+        label_2->setText(QApplication::translate("PhasePlateWindow", "Type of the phase plate:", 0));
         radioButton_simplePlate->setText(QString());
         radioButton_stagingPlate->setText(QString());
         radioButton_lensPlate->setText(QString());
         radioButton_flatLensPlate->setText(QString());
-        label_4->setText(QApplication::translate("PhasePlateWindow", "\320\237\320\276\320\273\321\203\320\262\320\276\320\273\320\275\320\276\320\262\320\260\321\217", 0));
-        label_5->setText(QApplication::translate("PhasePlateWindow", "\320\241\321\202\321\203\320\277\320\265\320\275\321\207\320\260\321\202\320\260\321\217", 0));
-        label_6->setText(QApplication::translate("PhasePlateWindow", "\320\237\320\273\320\276\321\201\320\272\320\276-\320\262\321\213\320\277\321\203\320\272\320\273\320\260\321\217 \320\273\320\270\320\275\320\267\320\260", 0));
-        label_7->setText(QApplication::translate("PhasePlateWindow", "\320\237\320\273\320\276\321\201\320\272\320\260\321\217 \320\273\320\270\320\275\320\267\320\260 \320\244\321\200\320\265\320\275\320\265\320\273\321\217", 0));
-        label_8->setText(QApplication::translate("PhasePlateWindow", "\320\220\320\274\320\277\320\273\320\270\321\202\321\203\320\264\320\260 \320\275\320\260 \320\276\321\201\320\270", 0));
-        pushButton_Back->setText(QApplication::translate("PhasePlateWindow", "\320\222\321\213\321\205\320\276\320\264", 0));
-        label->setText(QApplication::translate("PhasePlateWindow", "\320\244\320\260\320\267\320\276\320\262\320\260\321\217 \320\277\320\273\320\260\321\201\321\202\320\270\320\275\320\272\320\260", 0));
-        pushButton_Next->setText(QApplication::translate("PhasePlateWindow", "\320\222 \320\275\320\260\321\207\320\260\320\273\320\276", 0));
+        label_4->setText(QApplication::translate("PhasePlateWindow", "Half-wavelength", 0));
+        label_5->setText(QApplication::translate("PhasePlateWindow", "Stepwise", 0));
+        label_6->setText(QApplication::translate("PhasePlateWindow", "Plano-convex lens", 0));
+        label_7->setText(QApplication::translate("PhasePlateWindow", "Flat Fresnel lens", 0));
+        label_8->setText(QApplication::translate("PhasePlateWindow", "Amplitude at the axis", 0));
+        pushButton_Back->setText(QApplication::translate("PhasePlateWindow", "Quit", 0));
+        label->setText(QApplication::translate("PhasePlateWindow", "Phase plate", 0));
+        pushButton_Next->setText(QApplication::translate("PhasePlateWindow", "Menu", 0));
     } // retranslateUi
 
 };
